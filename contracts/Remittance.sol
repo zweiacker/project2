683a04fcf0b450c6892ddf9ebe315a674ac22c40
Marc Zweiacker <info@zweiacker.com>
2018-08-24 15:33:48 +0200

    incorp some of Xavier's comments

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -15,10 +15,11 @@ contract Remittance {
     mapping (bytes32 => Transferal) public transferals;
     // bytes32[] public transferalsIndex;
 
-    event hashHelperTX(bytes32 hashOut);
-    event loaded (address beneficiary, bytes32 puzzle, uint amount);
-    event unloaded (address beneficiary, bytes32 puzzle, uint amount);
-    event released (address beneficiary, bytes32 puzzle, uint amount);
+    event PauseControl (string message, uint timestamp);
+    event HashHelperTX(bytes32 hashOut);
+    event Loaded (address beneficiary, bytes32 puzzle, uint amount);
+    event Unloaded (address beneficiary, bytes32 puzzle, uint amount);
+    event Released (address beneficiary, bytes32 puzzle, uint amount);
 
     constructor() public {
         owner = msg.sender;
@@ -42,19 +43,20 @@ contract Remittance {
 
     function pauseContract() public isOwner isRunning {
         pause = true;
+        emit PauseControl("Paused", block.timestamp);
     }
 
     function resumeContract() public isOwner isPaused {
         pause = false;
-    }
+        emit PauseControl("Resumed", block.timestamp);
+        }
 
     function collect() public isPaused isOwner {
         msg.sender.transfer(address(this).balance);
     }
 
     function hashHelper(address a, string pw) public pure returns(bytes32) {
-        bytes32 res = keccak256(a, pw);
-        return res;
+        return keccak256(a, pw);
     }
     
     // In remix, this seems to be the only way to get hold of the return value of hashHelper
@@ -62,7 +64,7 @@ contract Remittance {
     // the txHashHelper function would not be there in production
     function txHashHelper(address a, string pw) public isRunning returns (bytes32) {
         bytes32 h = hashHelper(a, pw);
-        emit hashHelperTX (h);
+        emit HashHelperTX (h);
         return h;
     }
 
@@ -79,7 +81,7 @@ contract Remittance {
         // expiry is two minutes after creation just for testing in remix
         newTransferal.expiry = block.timestamp + 120;
         transferals[_puzzle] = newTransferal;
-        emit loaded (_beneficiary, _puzzle, msg.value);
+        emit Loaded (_beneficiary, _puzzle, msg.value);
     }
     
     function unloadTransfer(address _beneficiary, bytes32 _puzzle) public isOwner isRunning {
@@ -94,13 +96,13 @@ contract Remittance {
         uint v = transferals[_puzzle].value;
         transferals[_puzzle].value = 0;
         transferals[_puzzle].transferTo = 0; // transferals[_puzzle] marked as "empty"
-        emit unloaded (_beneficiary, _puzzle, v);
+        emit Unloaded (_beneficiary, _puzzle, v);
         owner.transfer(v);
     }
     
     function releaseTransfer(string pw) public isRunning {
         bytes32 p = keccak256(msg.sender, pw);
-        require(transferals[p].transferTo == msg.sender);
+        // require(transferals[p].transferTo == msg.sender);
         require(transferals[p].puzzle == p);
         require(transferals[p].value > 0);
         require(transferals[p].expiry >= block.timestamp);
@@ -108,7 +110,7 @@ contract Remittance {
         uint v = transferals[p].value;
         transferals[p].value = 0;
         transferals[p].transferTo = 0;  // transferals[p] marked as "empty"
-        emit released (msg.sender, p, v);
+        emit Released (msg.sender, p, v);
         msg.sender.transfer(v);
     }
 }
